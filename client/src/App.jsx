4657a6252d7e6f808b1b91d900ad1a167a818995
rajAditya-singh <indrmy.126@gmail.com>
2026-05-30 12:48:36 +0530

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -295,29 +295,61 @@ function App() {
 
       {/* BUTTON */}
 
-      {!isConversationActive.current ? (
+      {/* VOICE WAVE BUTTON */}
 
-        <button
-          className={`hero-btn ${
-            hasStarted
-              ? "move-down"
-              : ""
-          }`}
-          onClick={startConversation}
-        >
-          🎤 Start Conversation
-        </button>
+<div
+  className={`voice-container ${
+    hasStarted ? "move-down" : ""
+  }`}
+  onClick={
+    !isConversationActive.current
+      ? startConversation
+      : stopConversation
+  }
+>
 
-      ) : (
+  {!isConversationActive.current ? (
 
-        <button
-          className="voice-btn stop move-down"
-          onClick={stopConversation}
-        >
-          🛑 End Conversation
-        </button>
+    <div className="mic-button">
 
-      )}
+      <div className="mic-glow"></div>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="30"
+        height="30"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="white"
+        strokeWidth="2.2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
+        <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
+        <line x1="12" y1="19" x2="12" y2="23" />
+        <line x1="8" y1="23" x2="16" y2="23" />
+      </svg>
+
+    </div>
+
+  ) : (
+
+    <div className="voice-wave-wrapper">
+
+      <div className={`voice-wave ${isListening ? "active" : ""}`}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    </div>
+
+  )}
+
+</div>
 
     </div>
 
